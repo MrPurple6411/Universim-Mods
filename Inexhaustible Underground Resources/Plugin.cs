88387b9d7dc50f84c9c2ea82664a38f766bb92d2
MrPurple6411 <mrpurple6411@gmail.com>
2024-01-07 13:19:44 -0700

    Inexhaustible Underground Resources

---
+++ Inexhaustible Underground Resources/Plugin.cs
@@ -0,0 +1,36 @@
+﻿namespace Inexhaustible_Underground_Resources;
+
+using BepInEx;
+using BepInEx.Logging;
+using Game;
+using Game.Actors.Urban.Buildings;
+using HarmonyLib;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger { get; private set; }
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+
+        // Harmony patching
+        Harmony.CreateAndPatchAll(typeof(Plugin), MyPluginInfo.PLUGIN_GUID);
+
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    [HarmonyPatch(typeof(MineActor), nameof(MineActor.SubtractResource)), HarmonyPrefix]
+    public static bool SubtractResource_Prefix(MineActor __instance, ref int __result)
+    {
+        if (__instance._depositLayer == null || __instance._depositsIndexes == null)
+        {
+            return true;
+        }
+
+        __result = 0;
+        return false;
+    }
+}
