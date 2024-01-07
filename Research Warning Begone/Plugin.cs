72b22302caad46f7e785cb24901c225c9954db74
MrPurple6411 <mrpurple6411@gmail.com>
2024-01-07 06:40:06 -0700

    Research Warning Begone

---
+++ Research Warning Begone/Plugin.cs
@@ -0,0 +1,29 @@
+﻿namespace Research_Warning_Begone;
+
+using BepInEx;
+using BepInEx.Logging;
+using Game.UI;
+using HarmonyLib;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger { get; private set; }
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+
+        // Harmony patching
+        Harmony.CreateAndPatchAll(typeof(Plugin), MyPluginInfo.PLUGIN_GUID);
+
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    [HarmonyPatch(typeof(MainHud), nameof(MainHud.UpdateUnActivityResearchMessage)),HarmonyPrefix]
+    public static bool UpdateUnActivityResearchMessage_Prefix()
+    {
+        return false;
+    }
+}
